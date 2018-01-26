fa81f4c9a67061aac23fa097921194cc9c7c4076
vaibhav <da-vaibhav@users.noreply.github.com>
2018-01-26 19:37:34 +0530

    basic things working, returns an author name correctly, wrote resolver

--- schema.js
+++ schema.js
@@ -1,9 +1,48 @@
+const promisify = require('util').promisify;
 const fetch = require('node-fetch');
 const key = require('./keys').key;
-const url = `https://www.goodreads.com/author/show/18541?format=xml&key=${key}`;
+const parseXML = promisify(require('xml2js').parseString);
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString
+} = require('graphql');
 
-const x = fetch(url)
-.then(response =>  response.text())
-.then(data => {
-  console.log(data);
+const baseURL = `https://www.goodreads.com/author/show/`;
+const endPart = `?format=xml&key=${key}`;
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: '...',
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: xml => {
+        return xml.GoodreadsResponse.author[0].name[0];
+      }
+    }
+  })
+});
+
+module.exports = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: '...',
+
+    fields: () => ({
+      author: {
+        type: AuthorType,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve: (root, args) => fetch(`${baseURL}${args.id}${endPart}`)
+          .then(response => response.text())
+          .then(parseXML)
+          .then(jsData => {
+            return jsData;
+          })
+      }
+    })
+  })
 });
